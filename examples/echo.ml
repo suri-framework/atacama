042446bc7552ed42d8d61846e87ec2b433e0e8dc
Leandro Ostera <leandro@abstractmachines.dev>
2023-11-22 13:13:31 +0100

    feat: move example to apps

--- examples/echo.ml
+++ examples/echo.ml
@@ -3,28 +3,25 @@
 open Riot
 
 module Echo = struct
-  open Atacama.Handler
-  include Atacama.Handler.Default
+  module Server = struct
+    open Atacama.Handler
+    include Atacama.Handler.Default
 
-  type state = int
+    type state = int
 
-  let handle_connection _socket state =
-    Logger.info (fun f -> f "[%d] connection" state);
-    Continue (state + 1)
+    let handle_connection _socket state =
+      Logger.info (fun f -> f "[%d] new connection" state);
+      Continue (state + 1)
 
-  let handle_data data socket state =
-    Logger.info (fun f -> f "[%d] echo: %S" state (Bigstringaf.to_string data));
-    let (Ok _bytes) = Atacama.Socket.send socket data in
-    Continue (state + 1)
+    let handle_data data socket state =
+      Logger.info (fun f ->
+          f "[%d] echo: %S" state (Bigstringaf.to_string data));
+      let (Ok _bytes) = Atacama.Socket.send socket data in
+      Continue (state + 1)
+  end
+
+  let start () = Atacama.start_link ~port:2112 (module Server) 0
+  let name = "echo_server"
 end
 
-let main () =
-  Logger.set_log_level (Some Trace);
-  let (Ok _) = Logger.start () in
-  sleep 0.1;
-  Logger.info (fun f -> f "starting atacama");
-  let (Ok pid) = Atacama.start_link ~port:2112 (module Echo) 0 in
-  wait_pids [ pid ];
-  Logger.info (fun f -> f "closing down")
-
-let () = Riot.run @@ main
+let () = Riot.start ~apps:[ (module Logger); (module Echo) ] ()
