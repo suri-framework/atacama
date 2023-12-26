11dd11f588badea20720eb71186798d568cbe459
Gleb Pomykalov <glebpom@gmail.com>
2023-12-26 03:07:22 +0100

    bench: improve rust benchmarks (#5)

--- bench/rust-echo/src/main.rs
+++ bench/rust-echo/src/main.rs
@@ -1,35 +1,25 @@
+use std::net::TcpStream;
+use tokio::io::BufReader;
 use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::spawn;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("0.0.0.0:2112").await?;
+    let listener = TcpListener::bind("127.0.0.1:2112").await?;
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let (socket, _) = listener.accept().await?;
+        let std_stream = socket.into_std().unwrap();
+        let socket = socket2::Socket::from(std_stream);
+        socket.set_recv_buffer_size(1024 * 50).unwrap();
+        let std_stream: TcpStream = socket.into();
+        let mut socket = tokio::net::TcpStream::from_std(std_stream).unwrap();
 
         spawn(async move {
-            let mut buf = [0; 1024 * 50];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
+            let (r, mut w) = socket.split();
+            let mut r = BufReader::with_capacity(1024 * 500, r);
+            tokio::io::copy_buf(&mut r, &mut w).await?;
+            Ok::<_, std::io::Error>(())
         });
     }
 }
