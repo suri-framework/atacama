9e767d66366cde0e4a3c635cfab9906356465844
Leandro Ostera <leandro@abstractmachines.dev>
2024-01-02 17:14:49 +0100

    fix: expose connection.close

--- atacama/atacama.mli
+++ atacama/atacama.mli
@@ -6,6 +6,7 @@ module Connection : sig
   val send : t -> IO.Buffer.t -> (int, [> `Closed | `Eof ]) IO.result
   val receive : ?limit:int -> t -> (IO.Buffer.t, [> `Closed | `Eof ]) IO.result
   val negotiated_protocol : t -> string option
+  val close : t -> unit
 end
 
 (** An Atacama Handler determines how every connection handled by Atacama will
